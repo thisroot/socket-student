56dac5f356b7f294bb44e786c5861ac04d454a6b
User <User@LAPTOP-1OFSPH9I>
2016-10-20 18:09:23 +0300

    - equation plugin into text editor

--- index.js
+++ index.js
@@ -2,33 +2,16 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-//app.get('/', function(req, res){
-//  res.sendFile(__dirname + '/index.html');
-//});
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-    console.log({my:'privet'});
-  });
-});
-
-io.on('connection', function(socket){
-  console.log('privet');  
-  socket.on('struct', function(data){
-    io.emit('struct', data);
-    console.log(data);
   });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
